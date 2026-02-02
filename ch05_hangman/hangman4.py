860f753f72dabbea1deb17b2f3980b877d2c8cd2
maybeags <suren0709@gmail.com>
2026-02-02 10:46:08 +0900

    feat: stages list uploaded

---
+++ ch05_hangman/hangman4.py
@@ -0,0 +1,65 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''',
+'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+word_list = [ 'apple', 'banana', 'camel' ]
+chosen_word = random.choice(word_list)
+print(f'test word : {chosen_word}')
+
+display = []
+for _ in range(len(chosen_word)):
+    display.append('_')
